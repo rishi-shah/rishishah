022f1e63a6fc88252d4a905afa28dc8b367ba564
rishi-shah <rishishah120@gmail.com>
2020-06-18 19:43:15 +0530

    Added blog link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,6 +9,7 @@
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
         <!-- <b-nav-item href="/blog">Blog</b-nav-item> -->
+        <b-nav-item href="/blog">Blog</b-nav-item>
         <b-nav-item href="/resume">Resume</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
